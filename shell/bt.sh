347cf2e6aeed00fddc657c13210b0d432dc0ac87
豆豆 <doudou8doudou@outlook.com>
2026-08-07 14:54:21 +0800

    更新 bt.sh

--- shell/bt.sh
+++ shell/bt.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 yum -y install expect
-bash <(curl -sSL --header 'Content-Type: application/json;charset=UTF-8' 'https://gitee.com/api/v5/repos/wendy-tools/wdtools/raw/conf/c/c1.1.sh?access_token=e714ea221d82ae604117e8410d2d309e')
-expect <(curl -sSL --header 'Content-Type: application/json;charset=UTF-8' 'https://gitee.com/api/v5/repos/wendy-tools/wdtools/raw/conf/c/btsetup?access_token=e714ea221d82ae604117e8410d2d309e')
+bash <(curl -sSL 'https://raw.githubusercontent.com/dou-blog/blog/refs/heads/master/shell/bt/1.sh')
+expect <(curl -sSL 'https://raw.githubusercontent.com/dou-blog/blog/refs/heads/master/shell/bt/btsetup')
 
